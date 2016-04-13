051b547206caf6b3a21935336b660bbce2712bd0
robertweber95 <robertweber95@gmail.com>
2016-04-13 14:15:05 -0400

    Initial Commit

---
+++ src/controller/GameController.java
@@ -0,0 +1,25 @@
+
+package controller;
+import model.*;
+import view.*;
+
+/**
+ * The controlling class at the game.
+ * @author Robert Weber
+ * @author Billy Sorg 
+ * @author Michael Gallo
+ * @author Ben Gause, 
+ * @author Drake Smith
+ */
+public class GameController {
+
+	/**
+	 * Main method for running the game
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
